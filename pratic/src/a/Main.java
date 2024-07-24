140bc101d3776b2b4ae72ab9e46b73cf40d212d2
GustavoNMendes01 <gustavo.nmendes01@gmail.comm>
2024-07-24 15:29:41 -0300

    commit 2

--- pratic/src/a/Main.java
+++ pratic/src/a/Main.java
@@ -16,10 +16,38 @@ public class Main {
 		double measure = 53.234567;
 		
 		System.out.printf("%s which price is $%.2f%n", product1, price1);
-		System.out.printf("%s, which price is $ %.2f", product2, price2);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		
+		int y, x;
+		x = 5;
+		y = x - 2;
+		  
+		System.out.println(y);
+		System.out.println(x);
+		
+		int i;
+		double o;
+		i = 5;
+		o = i - 2;
+		  
+		System.out.println(i);
+		System.out.println(o);
+		
+		double b, B, h, area;
+		b = 6;
+		B = 8;
+		h = 5;
+		
+		area = (b + B)/ 2.0 * h;
+		
+		System.out.println(area);
+		
+		
+		
+		
 		
 		
 	}
-
+ 
 }
 
